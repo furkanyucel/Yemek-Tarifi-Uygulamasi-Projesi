8533bbc0ebc89e32b8b5e30b336cae90bb6979a1
furkanyucel <furkanyucelux@gmail.com>
2024-03-08 17:05:41 +0300

    Header

---
+++ src/components/UI/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Style from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={Style.main}>
+      <h1>Gurmeden Tarifler</h1>
+    </div>
+  )
+}
+
+export default Header
