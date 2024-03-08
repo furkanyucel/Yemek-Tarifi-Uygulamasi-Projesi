9812637d4cc1b2fb846f18441d3669f19dc91f1c
furkanyucel <furkanyucelux@gmail.com>
2024-03-08 17:02:37 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/UI/Header/Header'
+import RecipeList from "./components/Recipes/RecipeList"
+import Footer from './components/UI/Footer/Footer'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <RecipeList />
+    <Footer />
+    </>
+  )
+}
+
+export default App
